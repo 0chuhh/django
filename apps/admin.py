5e9dbb796b7e2ea481f63497c6802b6291f8b09b
0chuhh <vvlasovdo@mail.ru>
2022-10-28 11:10:46 +0900

    models

--- apps/admin.py
+++ apps/admin.py
@@ -1,3 +1,6 @@
 from django.contrib import admin
-
+from .models import Status,Category,Product
 # Register your models here.
+admin.site.register(Status)
+admin.site.register(Category)
+admin.site.register(Product)
--- apps/models.py
+++ apps/models.py
@@ -1,3 +1,41 @@
+from unittest.util import _MAX_LENGTH
 from django.db import models
 
 # Create your models here.
+
+class Category(models.Model):
+    name = models.CharField(max_length=50)
+    image = models.ImageField(upload_to='static/images', null=True)
+
+    class Meta:
+        verbose_name = 'Категория'
+        verbose_name_plural = 'Категории'
+
+    def __str__(self):
+        return self.name
+
+class Status(models.Model):
+    name = models.CharField(max_length=50)
+
+    class Meta:
+        verbose_name = 'Статус'
+        verbose_name_plural = 'Статусы'
+
+    def __str__(self):
+        return self.name
+
+class Product(models.Model):
+    name = models.CharField(max_length=100)
+    price = models.IntegerField()
+    image = models.ImageField(upload_to='static/images')
+    category = models.ForeignKey(Category, on_delete=models.CASCADE)
+    status = models.ForeignKey(Status, on_delete=models.CASCADE)
+    discount = models.IntegerField(null=True, )
+
+    class Meta:
+        verbose_name = 'Продукт'
+        verbose_name_plural = 'Продукты'
+
+    def __str__(self):
+        return self.name
+
